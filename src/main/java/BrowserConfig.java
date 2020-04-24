2076c4957aca61ea83c4f90eb06043dbc290432f
ojhfoi <gabov.roman@gmail.com>
2020-04-24 22:43:22 +0500

    new lesson

--- src/main/java/BrowserConfig.java
+++ src/main/java/BrowserConfig.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.webdriver.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -6,15 +7,18 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 
-public class BrowserConfig {
+public class BrowserConfig extends WebDriverFactory {
 
     private static WebDriver driver = null;    
 
     @BeforeSuite
     public void CreateDriver(){
-        String br = System.getProperty("browser").toUpperCase();
+//        String br = System.getProperty("browser").toUpperCase();
+//        if (br == null){
+//            br = "chrome";
+//        }
+        String br = "CHROME";
         setWebDriver(browser.valueOf(br).create());
-
     }
 
     @AfterSuite(description = "Close browser and driver")
--- src/test/java/ojhfoi/navigate/OpenSite.java
+++ src/test/java/ojhfoi/navigate/OpenSite.java
@@ -2,6 +2,9 @@ package ojhfoi.navigate;
 
 import ojhfoi.navigate.config.config;
 import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,14 +12,25 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class OpenSite {
 
+
+
     @Test(description = "run site")
     public void OpenSite() throws Throwable{
         try {
 
+            WebDriver driver = getWebDriver();
+
             config cfg = ConfigFactory.create(config.class);
 
-            getWebDriver().navigate().to(cfg.hostname());
-            System.out.println("Site is open! Site title:" + getWebDriver().getTitle());
+            driver.navigate().to(cfg.hostname());
+//            System.out.println("Site is open! Site title:" + getWebDriver().getTitle());
+
+            WebElement element = driver.findElement(By.xpath("//a[@title='Отзывы']"));
+            driver.navigate().refresh();
+            element.click();
+
+//            driver.findElement();
+
 
         } catch (Throwable throwable) {
             Assert.assertTrue(false, "Can't open site becouse: \n" + throwable.getCause());
